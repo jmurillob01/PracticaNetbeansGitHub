8ee56f1adf7c72437aca906eb1658ec9e1a44203
Javi <javiermb2302@gmail.com>
2022-03-12 19:33:11 +0100

    Carga inicial

---
+++ src/proyectoempresa/ProyectoEmpresa.java
@@ -0,0 +1,12 @@
+package proyectoempresa;
+
+/**
+ * @version 1.0
+ * @author Javi
+ */
+
+public class ProyectoEmpresa {
+    public static void main(String[] args) {
+        System.out.println("Este es un proyecto muy importante, fundamental no perderlo");
+    }
+}
