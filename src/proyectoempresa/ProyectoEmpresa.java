687f533ce5a32b412e12e497c917d259eba87949
Javi <javiermb2302@gmail.com>
2022-03-12 19:57:12 +0100

    Añadiendo suma

--- src/proyectoempresa/ProyectoEmpresa.java
+++ src/proyectoempresa/ProyectoEmpresa.java
@@ -8,5 +8,11 @@ package proyectoempresa;
 public class ProyectoEmpresa {
     public static void main(String[] args) {
         System.out.println("Este es un proyecto muy importante, fundamental no perderlo");
+        System.out.println("Ahora estamos realizando modificaciones");
+        
+        int x =  1;
+        int y = 2;
+        int suma = x + y;
+        System.out.println(suma);
     }
 }
